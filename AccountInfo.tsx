448d74cf7d249a73a5d15d6a5fc8b74ce1cb26f4
Matt Reetz <matthewcarlreetz@gmail.com>
2021-09-22 13:29:12 -0500

    Page through account activity.

--- AccountInfo.tsx
+++ AccountInfo.tsx
@@ -1,4 +1,4 @@
-import React, {memo, useState} from 'react';
+import React, {memo, useCallback, useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   Button,
@@ -6,10 +6,8 @@ import {
   ScrollView,
   StyleSheet,
   Text,
-  TextInput,
-  View,
 } from 'react-native';
-import {gql, useLazyQuery, useQuery} from '@apollo/client';
+import {gql, useLazyQuery} from '@apollo/client';
 import Input from './Input';
 
 const DATA_QUERY = gql`
@@ -32,6 +30,7 @@ const DATA_QUERY = gql`
       data {
         time
         type
+        hash
       }
     }
   }
@@ -39,12 +38,36 @@ const DATA_QUERY = gql`
 
 const AccountInfo = () => {
   const [address, setAddress] = useState('');
-  const [getData, {loading, data}] = useLazyQuery(DATA_QUERY);
-  console.log({loading, data, address});
+  const [cursor, setCursor] = useState('');
+  const [getData, {loading, data, fetchMore, error}] = useLazyQuery(
+    DATA_QUERY,
+    {
+      notifyOnNetworkStatusChange: true,
+    },
+  );
+  console.log({loading, data, address, cursor, error});
 
-  if (loading) {
-    return <ActivityIndicator color="black" />;
-  }
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+    const {
+      accountActivity: {cursor: nextCursor, data: innerData},
+    } = data;
+
+    console.log({dataLength: innerData.length});
+    setCursor(nextCursor);
+  }, [cursor, data]);
+
+  const handleTextChange = useCallback(text => {
+    setAddress(text);
+    setCursor('');
+  }, []);
+
+  const handleDataRequest = useCallback(() => {
+    console.log('handleDataRequest');
+    getData({variables: {address, cursor}});
+  }, [address, cursor, getData]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -54,17 +77,22 @@ const AccountInfo = () => {
           style={styles.inputContainer}
           inputProps={{
             editable: !loading,
-            onChangeText: setAddress,
+            onChangeText: handleTextChange,
             value: address,
             placeholder: 'Address',
             style: styles.input,
             multiline: true,
           }}
         />
+        <Button title="Get Account Data" onPress={handleDataRequest} />
         <Button
-          title="Get Account Data"
-          onPress={() => getData({variables: {address, cursor: ''}})}
+          title="Fetch More Activity"
+          onPress={() => {
+            console.log({fetchMore: !!fetchMore});
+            fetchMore?.({variables: {address, cursor}});
+          }}
         />
+        {loading && <ActivityIndicator color="black" />}
         <Text>{JSON.stringify(data, null, 2)}</Text>
       </ScrollView>
     </SafeAreaView>
--- App.tsx
+++ App.tsx
@@ -2,9 +2,31 @@ import React from 'react';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import AccountInfo from './AccountInfo';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        accountActivity: {
+          keyArgs: ['address'],
+          merge(existing, incoming) {
+            console.log({existing, incoming});
+            const {cursor, data} = incoming;
+            const existingData: any[] = existing?.data || [];
+            const nextData = [...data, ...existingData].filter(
+              (v, i, a) => a.findIndex(t => t.hash === v.hash) === i, // filter dups
+            );
+
+            return {cursor, data: nextData};
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphiql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 const App = () => {
