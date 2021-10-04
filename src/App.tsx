a2d13d6c403a5a91b069844bce50b39b51580ee7
Tyler Whitman <twhit093@gmail.com>
2021-10-04 13:00:29 -0700

    cloud backup (#9)
    * add ios cloud store
    
    * add cloud
    add CloudStorage module
    
    * update android app id

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React, { useMemo } from 'react'
 import { ApolloProvider } from '@apollo/client'
 import { Text, useColorScheme } from 'react-native'
 import { ThemeProvider } from '@shopify/restyle'
+import { useAsync } from 'react-async-hook'
 import AccountInfo from './AccountInfo'
 import { useApolloClient } from './graphql/useApolloClient'
 import { theme, darkThemeColors, lightThemeColors } from './theme/theme'
+import CloudStorage from './cloudStorage/CloudStorage'
 
 const App = () => {
   const { client, loading: loadingClient } = useApolloClient()
@@ -17,6 +19,15 @@ const App = () => {
     }),
     [colorScheme],
   )
+
+  // cloud storage test
+  useAsync(async () => {
+    const stored = await CloudStorage.getItem('test')
+    console.log(stored)
+
+    // uncomment and let this run once and you can test out the cloud storage for iOS
+    // await CloudStorage.setItem('test', 'this is a test')
+  }, [])
 
   if (!client || loadingClient) {
     return <Text>Splash Screen</Text>
