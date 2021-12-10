c7efd86d8ccb992274fe124755081a084ca3128f
Matt Reetz <matthewcarlreetz@gmail.com>
2021-12-10 09:21:38 -0600

    Revert numeric separator (#33)
    * Revert "Add babel numeric separator support (#32)"
    
    This reverts commit f425b37c82d617167992f5ef7f3f97b562ac45ed.
    
    * Revert "Revert "Remove numeric separators to appease app center. (#30)" (#31)"
    
    This reverts commit 0c07a6ffb208f2e1a7be22049966f6eb95e87083.

--- metro.config.js
+++ metro.config.js
@@ -13,7 +13,6 @@ module.exports = (async () => {
           inlineRequires: true,
         },
       }),
-      plugins: ['@babel/plugin-proposal-numeric-separator'],
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
