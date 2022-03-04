9e38e8d919c9e76d47364dbd335ed3b6ba25a634
Tyler Whitman <twhit093@gmail.com>
2022-03-04 12:15:04 -0800

    add missing pre build script vars (#104)

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,3 +2,5 @@
 
 # add required app center environment variables into a .env file for use with react-native-config
 echo GRAPH_URI=$GRAPH_URI >> .env
+echo ONE_SIGNAL_APP_ID=$ONE_SIGNAL_APP_ID >> .env
+echo ONE_SIGNAL_ACCOUNT_TAG_SALT=$ONE_SIGNAL_ACCOUNT_TAG_SALT >> .env
