82dd2605d927c3b2b3b4d5632d10cef0b879fdcc
Tyler Whitman <twhit093@gmail.com>
2022-05-03 14:37:55 -0700

    support utf8Memo qr param for legacy qr codes (#204)

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,3 +1,23 @@
 import { Buffer } from 'buffer'
 
 global.Buffer = Buffer
+
+// String.prototype.replaceAll() polyfill
+if (!String.prototype.replaceAll) {
+  // eslint-disable-next-line no-extend-native
+  String.prototype.replaceAll = function (str, newStr) {
+    // If a regex pattern
+    if (
+      Object.prototype.toString.call(str).toLowerCase() === '[object regexp]'
+    ) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      return this.replace(str, newStr)
+    }
+
+    // If a string
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return this.replace(new RegExp(str, 'g'), newStr)
+  }
+}
