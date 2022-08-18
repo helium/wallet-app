954b76a08f30052e27e24158fbc9940b982cc12d
Andrew Allen <allenan@helium.com>
2022-08-18 07:00:20 -0700

    use modal presentation more consistently (#274)
    * use modal presentation
    
    * ignore onesignal tagging if config not set

--- src/storage/oneSignalStorage.ts
+++ src/storage/oneSignalStorage.ts
@@ -3,12 +3,14 @@ import Config from 'react-native-config'
 import Bcrypt from 'bcrypt-react-native'
 
 export const tagAccount = async (address: string) => {
+  if (!Config.ONE_SIGNAL_ACCOUNT_TAG_SALT) return
   const salt = Config.ONE_SIGNAL_ACCOUNT_TAG_SALT
   const hash = await Bcrypt.hash(salt, address)
   OneSignal.sendTag(hash, ' ')
 }
 
 export const removeAccountTag = async (address: string) => {
+  if (!Config.ONE_SIGNAL_ACCOUNT_TAG_SALT) return
   const salt = Config.ONE_SIGNAL_ACCOUNT_TAG_SALT
   const hash = await Bcrypt.hash(salt, address)
   OneSignal.deleteTag(hash)
