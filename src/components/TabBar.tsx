8e4e08813e4da1ec843fa63c04ce24b036adb9e9
Matt Reetz <matthewcarlreetz@gmail.com>
2022-09-27 13:14:38 -0500

    Create tabbar component. (#298)

---
+++ src/components/TabBar.tsx
@@ -0,0 +1,118 @@
+import React, { memo, useCallback, useEffect, useState } from 'react'
+import { Insets, LayoutChangeEvent, LayoutRectangle } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
+import { useVerticalHitSlop } from '../theme/themeHooks'
+import Box from './Box'
+import Text from './Text'
+import TouchableOpacityBox from './TouchableOpacityBox'
+
+const TabBarItem = ({
+  title,
+  selected,
+  onLayout,
+  onPress,
+  hitSlop,
+}: {
+  title: string
+  selected: boolean
+  onPress: () => void
+  onLayout: (event: LayoutChangeEvent) => void
+  hitSlop: Insets | undefined
+}) => {
+  return (
+    <TouchableOpacityBox
+      onPress={onPress}
+      onLayout={onLayout}
+      marginRight="m"
+      hitSlop={hitSlop}
+    >
+      <Text
+        variant="subtitle2"
+        color={selected ? 'primaryText' : 'secondaryText'}
+        minWidth={75}
+        textAlign="center"
+      >
+        {title}
+      </Text>
+    </TouchableOpacityBox>
+  )
+}
+
+type Props = {
+  tabBarOptions: Array<{ title: string; value: string }>
+  selectedValue: string
+  onItemSelected: (value: string) => void
+}
+const TabBar = ({ tabBarOptions, selectedValue, onItemSelected }: Props) => {
+  const hitSlop = useVerticalHitSlop('l')
+  const [itemRects, setItemRects] = useState<Record<string, LayoutRectangle>>()
+
+  const offset = useSharedValue(0)
+
+  const handleLayout = useCallback(
+    (value: string) => (e: LayoutChangeEvent) => {
+      e.persist()
+
+      setItemRects((x) => ({ ...x, [value]: e.nativeEvent.layout }))
+    },
+    [],
+  )
+
+  const handlePress = useCallback(
+    (value: string) => () => {
+      onItemSelected(value)
+    },
+    [onItemSelected],
+  )
+
+  useEffect(() => {
+    const nextOffset = itemRects?.[selectedValue]?.x || 0
+
+    if (offset.value === 0) {
+      // Don't animate on first position update
+      offset.value = nextOffset
+      return
+    }
+
+    offset.value = withSpring(nextOffset, { mass: 0.5 })
+  }, [itemRects, offset.value, selectedValue])
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value }],
+    }
+  })
+
+  return (
+    <Box>
+      <Box flexDirection="row" justifyContent="center" paddingVertical="ms">
+        {tabBarOptions.map((o) => (
+          <TabBarItem
+            key={o.value}
+            title={o.title}
+            selected={o.value === selectedValue}
+            onLayout={handleLayout(o.value)}
+            onPress={handlePress(o.value)}
+            hitSlop={hitSlop}
+          />
+        ))}
+      </Box>
+      <Animated.View style={animatedStyles}>
+        <Box
+          backgroundColor="primaryText"
+          height={3}
+          position="absolute"
+          bottom={0.5}
+          width={itemRects?.[selectedValue]?.width || 0}
+        />
+      </Animated.View>
+      <Box backgroundColor="black200" height={1} width="100%" />
+    </Box>
+  )
+}
+
+export default memo(TabBar)
--- src/features/account/AccountsCarousel.tsx
+++ src/features/account/AccountsCarousel.tsx
@@ -1,15 +1,19 @@
+import { useNavigation } from '@react-navigation/native'
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Carousel } from 'react-native-snap-carousel'
+import TabBar from '../../components/TabBar'
 import { useAccountStorage } from '../../storage/AccountStorageProvider'
 import { CSAccount } from '../../storage/cloudStorage'
 import { useSpacing } from '../../theme/themeHooks'
 import { wp } from '../../utils/layout'
-import OnboardingSegment, {
-  OnboardingOpt,
-} from '../onboarding/multiAccount/OnboardingSegment'
+import { MultiAccountNavigationProp } from '../onboarding/multiAccount/MultiAccountNavigator'
+import { OnboardingOpt } from '../onboarding/multiAccount/OnboardingSegment'
 import AccountHeader from './AccountHeader'
 
 const AccountsCarousel = () => {
+  const { t } = useTranslation()
+  const navigation = useNavigation<MultiAccountNavigationProp>()
   const carouselRef = useRef<Carousel<CSAccount | null>>(null)
 
   const spacing = useSpacing()
@@ -36,20 +40,48 @@ const AccountsCarousel = () => {
     carouselRef.current?.snapToItem(index)
   }, [carouselData, currentAccount])
 
+  const segmentData = useMemo(() => {
+    const data = [
+      { value: 'import', title: t('onboarding.import') },
+      { value: 'ledger', title: t('onboarding.ledger') },
+      { value: 'create', title: t('onboarding.create') },
+    ]
+    return data
+  }, [t])
+
   const carouselWidths = useMemo(() => {
     const sliderWidth = wp(100)
     const itemWidth = sliderWidth - spacing.lx * 2
     return { sliderWidth, itemWidth }
   }, [spacing.lx])
 
+  const onItemSelected = useCallback(
+    (value: string) => {
+      const opt = value as OnboardingOpt
+      setOnboardingType(opt)
+
+      switch (opt) {
+        case 'create':
+          navigation.navigate('AccountCreateStart')
+          break
+        case 'import':
+          navigation.navigate('AccountImportStartScreen')
+          break
+        case 'ledger':
+          navigation.navigate('LedgerStart')
+          break
+      }
+    },
+    [navigation],
+  )
+
   const renderCarouselItem = ({ item }: { item: CSAccount | null }) => {
     if (!item) {
       return (
-        <OnboardingSegment
-          marginTop="s"
-          padding="m"
-          onSegmentChange={setOnboardingType}
-          onboardingType={onboardingType}
+        <TabBar
+          tabBarOptions={segmentData}
+          onItemSelected={onItemSelected}
+          selectedValue={onboardingType}
         />
       )
     }
