8a9ed21d936c84c99d50eec6b1da0af8aac77d04
Matt Reetz <matthewcarlreetz@gmail.com>
2022-10-21 09:42:43 -0500

    WIP

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
