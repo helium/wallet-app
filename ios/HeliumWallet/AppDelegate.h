5959ac47b30f8f243ecbc9e60e7491255f7dbb14
Tyler Whitman <twhit093@gmail.com>
2022-10-21 13:21:24 -0700

    ios fixes

--- ios/HeliumWallet/AppDelegate.h
+++ ios/HeliumWallet/AppDelegate.h
@@ -1,9 +1,8 @@
-#import <Foundation/Foundation.h>
 #import <React/RCTBridgeDelegate.h>
+#import <Expo/Expo.h>
 #import <UIKit/UIKit.h>
 
-#import <Expo/Expo.h>
-
-@interface AppDelegate : EXAppDelegateWrapper <RCTBridgeDelegate>
+@interface AppDelegate : EXAppDelegateWrapper <UIApplicationDelegate, RCTBridgeDelegate>
 
 @end
+
