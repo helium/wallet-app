bc2ecb0495a7459f4da34071e33ee5974295983c
Matt Reetz <matt@helium.com>
2022-10-24 12:00:34 -0500

    Feature/rn 70.3 (#10)
    Co-authored-by: Tyler Whitman <twhit093@gmail.com>

--- android/app/src/main/java/com/helium/wallet/app/MainActivity.java
+++ android/app/src/main/java/com/helium/wallet/app/MainActivity.java
@@ -1,5 +1,4 @@
 package com.helium.wallet.app;
-import expo.modules.ReactActivityDelegateWrapper;
 import com.facebook.react.ReactActivityDelegate;
 
 import com.facebook.react.ReactActivity;
@@ -10,8 +9,8 @@ import android.view.WindowManager;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is
-   * used to schedule rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
@@ -26,5 +25,36 @@ public class MainActivity extends ReactActivity {
         WindowManager.LayoutParams.FLAG_SECURE,
         WindowManager.LayoutParams.FLAG_SECURE
     );
+  }
+
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
+   * (Paper).
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  public static class MainActivityDelegate extends ReactActivityDelegate {
+    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+      super(activity, mainComponentName);
+    }
+
+    @Override
+    protected ReactRootView createRootView() {
+      ReactRootView reactRootView = new ReactRootView(getContext());
+      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+      return reactRootView;
+    }
+
+    @Override
+    protected boolean isConcurrentRootEnabled() {
+      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+      // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
   }
 }
--- ios/HeliumWallet/main.m
+++ ios/HeliumWallet/main.m
@@ -2,7 +2,8 @@
 #import "HeliumWallet-Swift.h"
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
