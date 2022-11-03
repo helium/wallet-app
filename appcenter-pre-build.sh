9f632765311efa97f3de274b8a73ae0c9177f41f
Matt Reetz <matt@helium.com>
2022-11-03 12:33:40 -0500

    Add missing api env var to pre build script. (#26)

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,7 @@
 
 # add required app center environment variables into a .env file for use with react-native-config
 echo GRAPH_URI=$GRAPH_URI >> .env
+echo WALLET_REST_URI=$WALLET_REST_URI >> .env
 echo ONE_SIGNAL_APP_ID=$ONE_SIGNAL_APP_ID >> .env
 echo ONE_SIGNAL_ACCOUNT_TAG_SALT=$ONE_SIGNAL_ACCOUNT_TAG_SALT >> .env
 echo SENTRY_DSN=$SENTRY_DSN >> .env
