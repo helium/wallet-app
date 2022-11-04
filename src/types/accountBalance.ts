8e8b3118415d2a3e2775b0950f225dba00308a4b
Matt Reetz <matt@helium.com>
2022-11-04 09:39:43 -0500

    Feature/solana balances chart (#23)
    * Show balance history for solana addresses.
    * Update max number of tokens visible.

---
+++ src/types/accountBalance.ts
@@ -0,0 +1,9 @@
+export type AccountBalance = {
+  hntBalance: number
+  stakedHntBalance: number
+  iotBalance: number
+  mobileBalance: number
+  date: string
+  hntPrice: number
+  balance: number
+}
