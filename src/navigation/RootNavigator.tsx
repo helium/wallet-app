cc2e731d794a8126792fb01099648c9c9c73f5cd
Luis Perrone <perronef5@users.noreply.github.com>
2022-11-27 14:36:52 -0500

    Only show tab bar navigator when in solana preview mode

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,19 +1,26 @@
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useMemo } from 'react'
 import changeNavigationBarColor from 'react-native-navigation-bar-color'
+import HomeNavigator from '../features/home/HomeNavigator'
 import { useColors } from '../theme/themeHooks'
 import { useAccountStorage } from '../storage/AccountStorageProvider'
 import OnboardingNavigator from '../features/onboarding/OnboardingNavigator'
 import TabBarNavigator from './TabBarNavigator'
+import { useAppStorage } from '../storage/AppStorageProvider'
 
 const RootNavigator = () => {
   const colors = useColors()
   const { hasAccounts } = useAccountStorage()
+  const { l1Network } = useAppStorage()
 
   useEffect(() => {
     changeNavigationBarColor(colors.primaryBackground, true, false)
   }, [colors.primaryBackground])
 
-  return hasAccounts ? <TabBarNavigator /> : <OnboardingNavigator />
+  const hasAccountsNavigator = useMemo(() => {
+    return l1Network === 'helium' ? <HomeNavigator /> : <TabBarNavigator />
+  }, [l1Network])
+
+  return hasAccounts ? hasAccountsNavigator : <OnboardingNavigator />
 }
 
 export default memo(RootNavigator)
