0550c5d865007574f3840f87ab56fde8c7bc9f12
Luis Perrone <perronef5@users.noreply.github.com>
2023-01-11 16:25:37 -0500

    Remove unused portal provider from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { PortalProvider } from '@gorhom/portal'
 import * as SplashLib from 'expo-splash-screen'
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { useApolloClient } from './graphql/useApolloClient'
 import { theme, darkThemeColors, lightThemeColors } from './theme/theme'
 import RootNavigator from './navigation/RootNavigator'
@@ -98,44 +97,42 @@ const App = () => {
       <SafeAreaProvider>
         <ThemeProvider theme={colorAdaptedTheme}>
           <SplashScreen>
-            <BottomSheetModalProvider>
-              <PortalProvider>
-                <OnboardingProvider>
-                  {client && (
-                    <ApolloProvider client={client}>
-                      <LockScreen>
-                        <SentinelScreen>
-                          <WalletConnectProvider>
-                            {accountsRestored && (
-                              <>
-                                <NavigationContainer
-                                  theme={navTheme}
-                                  linking={linking}
-                                  ref={navigationRef}
-                                >
-                                  <BalanceProvider>
-                                    <TransactionProvider>
-                                      <NetworkAwareStatusBar />
-                                      <RootNavigator />
-                                    </TransactionProvider>
-                                  </BalanceProvider>
-                                </NavigationContainer>
-                                <SecurityScreen
-                                  visible={
-                                    appState !== 'active' &&
-                                    appState !== 'unknown'
-                                  }
-                                />
-                              </>
-                            )}
-                          </WalletConnectProvider>
-                        </SentinelScreen>
-                      </LockScreen>
-                    </ApolloProvider>
-                  )}
-                </OnboardingProvider>
-              </PortalProvider>
-            </BottomSheetModalProvider>
+            <PortalProvider>
+              <OnboardingProvider>
+                {client && (
+                  <ApolloProvider client={client}>
+                    <LockScreen>
+                      <SentinelScreen>
+                        <WalletConnectProvider>
+                          {accountsRestored && (
+                            <>
+                              <NavigationContainer
+                                theme={navTheme}
+                                linking={linking}
+                                ref={navigationRef}
+                              >
+                                <BalanceProvider>
+                                  <TransactionProvider>
+                                    <NetworkAwareStatusBar />
+                                    <RootNavigator />
+                                  </TransactionProvider>
+                                </BalanceProvider>
+                              </NavigationContainer>
+                              <SecurityScreen
+                                visible={
+                                  appState !== 'active' &&
+                                  appState !== 'unknown'
+                                }
+                              />
+                            </>
+                          )}
+                        </WalletConnectProvider>
+                      </SentinelScreen>
+                    </LockScreen>
+                  </ApolloProvider>
+                )}
+              </OnboardingProvider>
+            </PortalProvider>
           </SplashScreen>
         </ThemeProvider>
       </SafeAreaProvider>
