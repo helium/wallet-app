4be08f23463c4dbbf53a29077d10b467248741e5
Chewing Glass <chewinglass123@gmail.com>
2023-02-15 22:06:42 -0600

    Add constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 import { HNT_MINT, IOT_MINT, MOBILE_MINT } from '@helium/spl-utils'
 
 import { lazyDistributorKey } from '@helium/lazy-distributor-sdk'
+=======
+import { lazyDistributorKey } from '@helium/lazy-distributor-sdk'
+import { HNT_MINT, IOT_MINT, MOBILE_MINT } from '@helium/spl-utils'
+>>>>>>> 3a3cbc7 (Add constants)
 import { PublicKey } from '@solana/web3.js'
 
 export const Mints: Record<string, string> = {
@@ -8,9 +13,15 @@ export const Mints: Record<string, string> = {
   MOBILE: MOBILE_MINT.toBase58(),
   HNT: HNT_MINT.toBase58(),
 }
+<<<<<<< HEAD
 
 export const MOBILE_LAZY_KEY = lazyDistributorKey(
   new PublicKey(Mints.MOBILE),
 )[0]
 
+=======
+export const MOBILE_LAZY_KEY = lazyDistributorKey(
+  new PublicKey(Mints.MOBILE),
+)[0]
+>>>>>>> 3a3cbc7 (Add constants)
 export const IOT_LAZY_KEY = lazyDistributorKey(new PublicKey(Mints.IOT))[0]
