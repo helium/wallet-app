5cb8c14fed326146dab9aa44afa03f50dad55e2c
Chewing Glass <chewinglass123@gmail.com>
2023-04-17 10:05:22 -0500

    Rephrase sign out to delete account

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -19,7 +19,7 @@ export default {
   },
   accountImport: {
     accountLimit:
-      'You have reached the account limit.\nTo add another account, sign out of a wallet account and try again.',
+      'You have reached the account limit.\nTo add another account, delete another wallet account and try again.',
     accountLimitLedger:
       'You have reached the account limit.\nTo add another account, uncheck an account and try again.',
     alert: {
@@ -421,10 +421,10 @@ export default {
   auth: {
     enterCurrent: 'Enter your current PIN to continue',
     error: 'Incorrect PIN',
-    signOut: 'Sign Out',
+    signOut: 'Delete Account',
     signOutAlert: {
-      body: 'You are signing out of all your accounts. Do you have your recovery words? If you don’t, you will lose access to:\n\n- your Address Book\n- your HNT\n- your Wallet',
-      title: 'Warning! Sign out of all accounts?',
+      body: 'You are deleting of all your accounts. Do you have your recovery words? If you don’t, you will lose access to:\n\n- your Address Book\n- your HNT\n- your Wallet',
+      title: 'Warning! Delete all accounts?',
     },
     title: 'Enter Your PIN',
   },
@@ -767,7 +767,7 @@ export default {
         body: 'Would you like to reveal your accounts words?',
         title: 'Reveal Words',
       },
-      title: 'Confirm Your\nWords To Sign Out',
+      title: 'Confirm Your\nWords To Delete Account Data.',
     },
     revealPrivateKey: {
       alertMessage:
@@ -792,14 +792,14 @@ export default {
         copyAddress: 'Copy Address',
         revealWords: 'Reveal Words',
         shareAddress: 'Share Address',
-        signOut: 'Sign Out',
+        signOut: 'Delete Account',
         signOutAlert: {
-          body: 'You are signing out of your account, {{alias}}. Do you have your recovery words? If you don’t, you will lose access to:\n\n- your HNT\n- your Wallet',
+          body: 'You are deleting your account, {{alias}}. Do you have your recovery words? If you don’t, you will lose access to:\n\n- your HNT\n- your Wallet',
           bodyLastAccount:
-            'You are signing out of your only account, {{alias}}. Do you have your recovery words? If you don’t, you will lose access to:\n\n- your Address Book\n- your HNT\n- your Wallet',
+            'You are deleting your only account, {{alias}}. Do you have your recovery words? If you don’t, you will lose access to:\n\n- your Address Book\n- your HNT\n- your Wallet',
           iCloudMessage:
             '\n\nAny device using the same iCloud account will also be signed out.',
-          title: 'Sign Out of {{alias}}?',
+          title: 'Delete {{alias}}?',
         },
         title: '{{alias}} Account Settings',
         migrateWallet: 'Migrate Wallet',
