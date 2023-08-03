50b7392bf711eab9782710e771e779ffe2f1303a
Chewing Glass <chewinglass123@gmail.com>
2023-08-03 11:48:58 -0500

    Simplify balance hook

--- src/utils/Balance.tsx
+++ src/utils/Balance.tsx
@@ -1,18 +1,15 @@
 import Balance, {
-  CurrencyType,
   DataCredits,
   IotTokens,
   MobileTokens,
   NetworkTokens,
   SolTokens,
   TestNetworkTokens,
-  Ticker,
 } from '@helium/currency'
 import { getOraclePrice } from '@helium/currency-utils'
-import { DC_MINT, HNT_MINT, IOT_MINT, MOBILE_MINT } from '@helium/spl-utils'
-import { PublicKey } from '@solana/web3.js'
+import { DC_MINT, HNT_MINT, IOT_MINT, MOBILE_MINT, toNumber } from '@helium/spl-utils'
+import { LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js'
 import BN from 'bn.js'
-import { round } from 'lodash'
 import React, {
   ReactNode,
   createContext,
@@ -35,7 +32,6 @@ import { useAppDispatch } from '../store/store'
 import { AccountBalance, BalanceInfo, TokenAccount } from '../types/balance'
 import StoreAtaBalance from './StoreAtaBalance'
 import StoreSolBalance from './StoreSolBalance'
-import { accountCurrencyType } from './accountUtils'
 import { decimalSeparator, groupSeparator } from './i18n'
 import { humanReadable } from './solanaUtils'
 import { useBalanceHistory } from './useBalanceHistory'
@@ -57,7 +53,7 @@ const useBalanceHook = () => {
   )
   const allBalances = useSelector((state: RootState) => state.balances.balances)
 
-  const { convertToCurrency, currency: currencyRaw } = useAppStorage()
+  const { currency: currencyRaw } = useAppStorage()
 
   const currency = useMemo(() => currencyRaw?.toLowerCase(), [currencyRaw])
 
@@ -100,9 +96,7 @@ const useBalanceHook = () => {
   const solanaPrice = useMemo(() => {
     if (!tokenPrices?.solana) return
 
-    const price = tokenPrices.solana[currency]
-
-    return new Balance(price, CurrencyType.usd)
+    return tokenPrices.solana[currency]
   }, [currency, tokenPrices])
 
   useEffect(() => {
@@ -127,70 +121,11 @@ const useBalanceHook = () => {
     [hntToDcPrice],
   )
 
-  const floatToBalance = useCallback(
-    (value: number, ticker: Ticker) => {
-      if (!currentAccount) {
-        console.warn('Cannot convert float to balance for nil account')
-        return
-      }
-      return Balance.fromFloatAndTicker(value, ticker)
-    },
-    [currentAccount],
-  )
-
-  const bonesToBalance = useCallback(
-    (v: number | undefined | null, ticker: Ticker | null | undefined) => {
-      return Balance.fromIntAndTicker(v || 0, ticker || 'HNT')
-    },
-    [],
-  )
-
-  const intToBalance = useCallback(
-    (opts: { intValue?: number }) => {
-      if (!opts.intValue === undefined || !currentAccount) {
-        console.warn('Cannot convert int to balance')
-        return
-      }
-      return new Balance(
-        opts.intValue,
-        accountCurrencyType(currentAccount.address, undefined),
-      )
-    },
-    [currentAccount],
-  )
-
-  const toPreferredCurrencyString = useCallback(
-    (
-      balance?: Balance<DataCredits | NetworkTokens | TestNetworkTokens>,
-      opts?: { maxDecimalPlaces?: number; showTicker?: boolean },
-    ): Promise<string> => {
-      if (!balance) {
-        return new Promise<string>((resolve) => resolve(''))
-      }
-      const multiplier = tokenPrices?.helium[currency] || 0
-
-      const showAsHnt =
-        !convertToCurrency ||
-        !multiplier ||
-        balance?.type.ticker === CurrencyType.dataCredit.ticker ||
-        balance?.type.ticker === CurrencyType.testNetworkToken.ticker
-
-      if (!showAsHnt) {
-        const convertedValue = multiplier * (balance?.floatBalance || 0)
-        return CurrencyFormatter.format(convertedValue, currency)
-      }
-      return new Promise<string>((resolve) =>
-        resolve(balanceToString(balance, opts)),
-      )
-    },
-    [convertToCurrency, currency, tokenPrices],
-  )
-
   const getBalance = useCallback(
     (mint: PublicKey, atas: Required<TokenAccount>[]) => {
       const mintStr = mint.toBase58()
       const ata = atas.find((a) => a.mint === mintStr)
-      return ata?.balance || 0
+      return new BN(ata?.balance || 0)
     },
     [],
   )
@@ -205,37 +140,24 @@ const useBalanceHook = () => {
 
     const solToken = accountBalancesForCluster?.sol
 
-    const solBalance = Balance.fromIntAndTicker(solToken?.balance || 0, 'SOL')
+    const solBalance = solToken?.balance
     const solPrice = tokenPrices?.solana?.[currency] || 0
-    const solAmount = solBalance?.floatBalance
-    const solValue = solPrice * solAmount
+    const solValue = solPrice * (solBalance / LAMPORTS_PER_SOL)
     const formattedSolValue = await CurrencyFormatter.format(solValue, currency)
 
-    const hntBalance = Balance.fromIntAndTicker(
-      getBalance(HNT_MINT, atas),
-      'HNT',
-    )
+    const hntBalance = getBalance(HNT_MINT, atas)
     const hntPrice = tokenPrices?.helium?.[currency] || 0
-    const hntAmount = hntBalance?.floatBalance
-    const hntValue = hntPrice * hntAmount
+    const hntValue = hntPrice * toNumber(hntBalance, 8)
     const formattedHntValue = await CurrencyFormatter.format(hntValue, currency)
 
-    const iotBalance = Balance.fromIntAndTicker(
-      getBalance(IOT_MINT, atas),
-      'IOT',
-    )
+    const iotBalance = getBalance(IOT_MINT, atas)
     const iotPrice = tokenPrices?.['helium-iot']?.[currency] || 0
-    const iotAmount = iotBalance?.floatBalance
-    const iotValue = iotPrice * iotAmount
+    const iotValue = iotPrice * toNumber(iotBalance, 6)
     const formattedIotValue = await CurrencyFormatter.format(iotValue, currency)
 
-    const mobileBalance = Balance.fromIntAndTicker(
-      getBalance(MOBILE_MINT, atas),
-      'MOBILE',
-    )
+    const mobileBalance = getBalance(MOBILE_MINT, atas)
     const mobilePrice = tokenPrices?.['helium-mobile']?.[currency] || 0
-    const mobileAmount = mobileBalance?.floatBalance
-    const mobileValue = mobilePrice * mobileAmount
+    const mobileValue = mobilePrice * toNumber(mobileBalance, 6)
     const formattedMobileValue = await CurrencyFormatter.format(
       mobileValue,
       currency,
@@ -257,15 +179,6 @@ const useBalanceHook = () => {
       formattedMobileValue,
       formattedSolValue,
       formattedTotal,
-      hntBalance,
-      hntValue,
-      iotBalance,
-      iotValue,
-      mobileBalance,
-      mobileValue,
-      solBalance,
-      solToken,
-      solValue,
     }
   }, [
     allBalances,
@@ -300,96 +213,34 @@ const useBalanceHook = () => {
     setBalanceInfo(tokenInfo)
   }, [cluster, prevCluster, prevSolAddress, solanaAddress, tokenInfo])
 
-  const toCurrencyString = useCallback(
-    (
-      balance: Balance<NetworkTokens | TestNetworkTokens>,
-      ticker: Ticker = 'HNT',
-    ): Promise<string> => {
-      const defaultResponse = new Promise<string>((resolve) => resolve(''))
-
-      const bal = Balance.fromIntAndTicker(balance.integerBalance, ticker)
-
-      let value = 0
-      switch (ticker) {
-        case 'HNT':
-          value = tokenPrices?.helium[currency] || 0
-          break
-        case 'SOL':
-          value = tokenPrices?.solana[currency] || 0
-          break
-        case 'IOT':
-          value = tokenPrices?.['helium-iot'][currency] || 0
-          break
-        case 'MOBILE':
-          value = tokenPrices?.['helium-mobile'][currency] || 0
-          break
-      }
-
-      if (!value) return defaultResponse
-
-      return CurrencyFormatter.format(value * bal.floatBalance, currency)
-    },
-    [currency, tokenPrices],
-  )
-
-  const toUsd = useCallback(
-    (balance?: Balance<NetworkTokens | TestNetworkTokens>): number => {
-      if (!balance) {
-        return 0
-      }
-      const multiplier = tokenPrices?.helium?.usd || 0
-
-      return round(multiplier * (balance?.floatBalance || 0), 2)
-    },
-    [tokenPrices],
-  )
-
   return {
     balanceHistory,
-    bonesToBalance,
     dcToNetworkTokens,
-    floatToBalance,
     ...balanceInfo,
-    intToBalance,
     networkTokensToDc,
     oracleDateTime,
     oraclePrice: hntToDcPrice,
     solanaPrice,
-    toCurrencyString,
-    toPreferredCurrencyString,
-    toUsd,
     tokenAccounts,
   }
 }
 
 const initialState = {
   balanceHistory: [] as AccountBalance[],
-  bonesToBalance: () => new Balance(0, CurrencyType.networkToken),
   dcToNetworkTokens: () => undefined,
-  floatToBalance: () => undefined,
   formattedDcValue: '',
   formattedHntValue: '',
   formattedIotValue: '',
   formattedMobileValue: '',
   formattedSolValue: '',
   formattedTotal: undefined,
-  hntBalance: new Balance(0, CurrencyType.networkToken),
-  intToBalance: () => undefined,
-  iotBalance: new Balance(0, CurrencyType.iot),
-  mobileBalance: new Balance(0, CurrencyType.mobile),
   networkTokensToDc: () => undefined,
   oracleDateTime: undefined,
   oraclePrice: undefined,
   solanaPrice: undefined,
-  solBalance: new Balance(0, CurrencyType.solTokens),
   solBalancesLoading: false,
-  toCurrencyString: () => new Promise<string>((resolve) => resolve('')),
-  toPreferredCurrencyString: () =>
-    new Promise<string>((resolve) => resolve('')),
-  toUsd: () => 0,
   atas: [],
   updating: false,
-  solToken: undefined,
   tokenAccounts: undefined,
 }
 const BalanceContext =
