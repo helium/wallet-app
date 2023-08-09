2eb1cdd69d322973a77ae34b76cbb136c11758d2
bry <bryanzettler@gmail.com>
2023-08-09 12:04:45 -0500

    Asserting with gain and height + dismissable keyboard

--- src/hooks/useIotInfo.ts
+++ src/hooks/useIotInfo.ts
@@ -4,8 +4,7 @@ import {
   iotInfoKey,
   rewardableEntityConfigKey,
 } from '@helium/helium-entity-manager-sdk'
-import { useIdlAccount } from '@helium/helium-react-hooks'
-import { IDL } from '@helium/idls/lib/esm/helium_entity_manager'
+import { useAnchorAccount } from '@helium/helium-react-hooks'
 import { HeliumEntityManager } from '@helium/idls/lib/types/helium_entity_manager'
 import { PublicKey } from '@solana/web3.js'
 import { IOT_SUB_DAO_KEY } from '@utils/constants'
@@ -21,11 +20,8 @@ export const useIotInfo = (
 ): UseAccountState<IotHotspotInfoV0> | undefined => {
   const [iotConfigKey] = rewardableEntityConfigKey(IOT_SUB_DAO_KEY, 'IOT')
   const [iotInfo] = iotInfoKey(iotConfigKey, entityKey || '')
+
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  return useIdlAccount<HeliumEntityManager>(
-    iotInfo,
-    IDL as HeliumEntityManager,
-    type,
-  )
+  return useAnchorAccount<HeliumEntityManager, type>(iotInfo, type)
 }
--- src/hooks/useMobileInfo.ts
+++ src/hooks/useMobileInfo.ts
@@ -4,8 +4,7 @@ import {
   mobileInfoKey,
   rewardableEntityConfigKey,
 } from '@helium/helium-entity-manager-sdk'
-import { useIdlAccount } from '@helium/helium-react-hooks'
-import { IDL } from '@helium/idls/lib/esm/helium_entity_manager'
+import { useAnchorAccount } from '@helium/helium-react-hooks'
 import { HeliumEntityManager } from '@helium/idls/lib/types/helium_entity_manager'
 import { PublicKey } from '@solana/web3.js'
 import { MOBILE_SUB_DAO_KEY } from '@utils/constants'
@@ -24,11 +23,8 @@ export const useMobileInfo = (
     'MOBILE',
   )
   const [mobileInfo] = mobileInfoKey(mobileConfigKey, entityKey || '')
+
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  return useIdlAccount<HeliumEntityManager>(
-    mobileInfo,
-    IDL as HeliumEntityManager,
-    type,
-  )
+  return useAnchorAccount<HeliumEntityManager, type>(mobileInfo, type)
 }
