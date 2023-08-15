c6f92bfe4700785bf949b34a99ee4833dd3fcd9c
Bryan <bryanzettler@gmail.com>
2023-08-15 16:23:36 -0500

    Fix useKeyToAsset (#427)

--- src/hooks/useKeyToAsset.ts
+++ src/hooks/useKeyToAsset.ts
@@ -1,21 +1,18 @@
-import {
-  mobileInfoKey,
-  rewardableEntityConfigKey,
-} from '@helium/helium-entity-manager-sdk'
+import { keyToAssetForAsset } from '@helium/helium-entity-manager-sdk'
 import { useAnchorAccount } from '@helium/helium-react-hooks'
 import { HeliumEntityManager } from '@helium/idls/lib/types/helium_entity_manager'
-import { MOBILE_SUB_DAO_KEY } from '@utils/constants'
+import { DAO_KEY } from '@utils/constants'
+import { toAsset } from '@utils/solanaUtils'
+import { CompressedNFT, HotspotWithPendingRewards } from '../types/solana'
 
 const type = 'keyToAssetV0'
-
-export const useKeyToAsset = (entityKey: string | undefined) => {
-  const [mobileConfigKey] = rewardableEntityConfigKey(
-    MOBILE_SUB_DAO_KEY,
-    'MOBILE',
+export const useKeyToAsset = (hotspot: HotspotWithPendingRewards) => {
+  const keyToAssetKey = keyToAssetForAsset(
+    toAsset(hotspot as CompressedNFT),
+    DAO_KEY,
   )
-  const [mobileInfo] = mobileInfoKey(mobileConfigKey, entityKey || '')
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  return useAnchorAccount<HeliumEntityManager, type>(mobileInfo, type)
+  return useAnchorAccount<HeliumEntityManager, type>(keyToAssetKey, type)
 }
