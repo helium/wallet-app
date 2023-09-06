9514581c7f57a5c7d755b25b6b4ca27e92591cd8
Chewing Glass <chewinglass123@gmail.com>
2023-09-06 11:07:10 -0500

    Add ability to refresh tokens in case of uncaught balance changes

--- src/features/account/AccountTokenList.tsx
+++ src/features/account/AccountTokenList.tsx
@@ -3,15 +3,24 @@ import Text from '@components/Text'
 import TouchableOpacityBox from '@components/TouchableOpacityBox'
 import { BottomSheetFlatList } from '@gorhom/bottom-sheet'
 import { BottomSheetFlatListProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetScrollable/types'
+import { useAccountFetchCache } from '@helium/account-fetch-cache-hooks'
 import { DC_MINT, HNT_MINT, IOT_MINT, MOBILE_MINT } from '@helium/spl-utils'
 import { useNavigation } from '@react-navigation/native'
+import { getAssociatedTokenAddressSync } from '@solana/spl-token'
 import { PublicKey } from '@solana/web3.js'
-import { useVisibleTokens, DEFAULT_TOKENS } from '@storage/TokensProvider'
+import { useAccountStorage } from '@storage/AccountStorageProvider'
+import { DEFAULT_TOKENS, useVisibleTokens } from '@storage/TokensProvider'
+import { useColors } from '@theme/themeHooks'
 import { useBalance } from '@utils/Balance'
 import { times } from 'lodash'
-import React, { useCallback, useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo } from 'react'
+import { useAsyncCallback } from 'react-async-hook'
 import { useTranslation } from 'react-i18next'
+import { AppState, RefreshControl } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useSolana } from '../../solana/SolanaProvider'
+import { syncTokenAccounts } from '../../store/slices/balancesSlice'
+import { useAppDispatch } from '../../store/store'
 import { HomeNavigationProp } from '../home/homeTypes'
 import TokenListItem, { TokenSkeleton } from './TokenListItem'
 
@@ -33,6 +42,46 @@ const AccountTokenList = ({ onLayout }: Props) => {
   const navigation = useNavigation<HomeNavigationProp>()
   const { t } = useTranslation()
   const { visibleTokens } = useVisibleTokens()
+  const { currentAccount } = useAccountStorage()
+  const dispatch = useAppDispatch()
+  const { anchorProvider, cluster } = useSolana()
+  const colors = useColors()
+  const cache = useAccountFetchCache()
+
+  const { loading: refetchingTokens, execute: refetchTokens } =
+    useAsyncCallback(async () => {
+      if (!anchorProvider || !currentAccount || !cluster) return
+      await dispatch(
+        syncTokenAccounts({ cluster, acct: currentAccount, anchorProvider }),
+      )
+      await Promise.all(
+        [...visibleTokens].map((mintStr) => {
+          // Trigger a refetch on all visible token accounts
+          return cache.search(
+            getAssociatedTokenAddressSync(
+              new PublicKey(mintStr),
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              new PublicKey(currentAccount!.solanaAddress!),
+            ),
+            undefined,
+            false,
+            true,
+          )
+        }),
+      )
+    })
+
+  // Trigger refresh when the app comes into the foreground from the background
+  useEffect(() => {
+    const listener = AppState.addEventListener('change', (state) => {
+      if (state === 'active') {
+        refetchTokens()
+      }
+    })
+    return () => {
+      listener.remove()
+    }
+  }, [refetchTokens])
 
   const onManageTokenList = useCallback(() => {
     navigation.navigate('AccountManageTokenListScreen')
@@ -107,6 +156,17 @@ const AccountTokenList = ({ onLayout }: Props) => {
       onLayout={onLayout}
       data={mints}
       numColumns={2}
+      refreshControl={
+        <RefreshControl
+          enabled
+          refreshing={refetchingTokens}
+          onRefresh={refetchTokens}
+          title=""
+          tintColor={colors.primaryText}
+        />
+      }
+      refreshing={refetchingTokens}
+      onRefresh={refetchTokens}
       columnWrapperStyle={{
         flexDirection: 'column',
       }}
