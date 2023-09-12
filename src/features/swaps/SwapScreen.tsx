5b99aeaa25e56a491ecde76c06dd8bf09e816de7
bry <bryanzettler@gmail.com>
2023-09-12 16:34:43 -0500

    allow only the right mints to be selected

--- src/features/swaps/SwapScreen.tsx
+++ src/features/swaps/SwapScreen.tsx
@@ -260,22 +260,25 @@ const SwapScreen = () => {
     [refresh, selectorMode, youReceiveMint],
   )
 
+  const validYouReceiveMints = useMemo(() => {
+    if (youPayMint.equals(HNT_MINT)) return [DC_MINT]
+    return [HNT_MINT, DC_MINT]
+  }, [youPayMint])
+
   const tokenData = useMemo(() => {
     const tokens = {
       [SelectorMode.youPay]: [MOBILE_MINT, HNT_MINT, IOT_MINT].map((mint) => ({
         mint,
         selected: youPayMint.equals(mint),
       })),
-      [SelectorMode.youReceive]: [MOBILE_MINT, HNT_MINT, IOT_MINT].map(
-        (mint) => ({
-          mint,
-          selected: youReceiveMint.equals(mint),
-        }),
-      ),
+      [SelectorMode.youReceive]: validYouReceiveMints.map((mint) => ({
+        mint,
+        selected: youReceiveMint.equals(mint),
+      })),
     }
 
     return tokens[selectorMode]
-  }, [selectorMode, youPayMint, youReceiveMint])
+  }, [selectorMode, youPayMint, youReceiveMint, validYouReceiveMints])
 
   const onCurrencySelect = useCallback(
     (youPay: boolean) => () => {
