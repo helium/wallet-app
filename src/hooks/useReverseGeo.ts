1321dbee0fe9b6d94e73878a21c191bffd70fc1c
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2023-10-06 16:32:35 -0400

    fix(#496): Initial location centering fix, and fix data only maker not found (#497)

--- src/hooks/useReverseGeo.ts
+++ src/hooks/useReverseGeo.ts
@@ -1,20 +1,39 @@
+import { truthy } from '@helium/spl-utils'
 import axios from 'axios'
 import { useAsync } from 'react-async-hook'
+
+const cache = new Map<string, Promise<string>>()
 
 export const useReverseGeo = (coords: number[] | undefined) =>
   useAsync(async () => {
     if (!coords) return ''
 
-    try {
-      const response = await axios.get(
-        `https://photon.komoot.io/reverse?lon=${coords[0]}&lat=${coords[1]}`,
-      )
-      const {
-        properties: { city, state, street },
-      } = response.data.features[0] || {}
+    const cacheKey = coords.join(',')
 
-      return `${street}, ${city}, ${state}`
-    } catch (e: any) {
-      console.error(e)
+    if (cache.has(cacheKey)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return cache.get(cacheKey)!
     }
+
+    const request = axios
+      .get(`https://photon.komoot.io/reverse?lon=${coords[0]}&lat=${coords[1]}`)
+      .then((response) => {
+        const { city, state, street } =
+          (response.data.features[0] || {}).properties || {}
+
+        const address = [street, city, state].filter(truthy).join(', ')
+
+        cache.delete(cacheKey) // Remove from cache after resolving
+
+        return address
+      })
+      .catch((e) => {
+        console.error(e)
+        cache.delete(cacheKey) // Remove from cache after rejecting
+        throw e
+      })
+
+    cache.set(cacheKey, request)
+
+    return request
   }, [coords])
