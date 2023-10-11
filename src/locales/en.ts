96fbe9996df88fbba016deb73858bd26f7cee0e1
Chewing Glass <chewinglass123@gmail.com>
2023-10-11 10:18:06 -0400

    Change text

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -170,7 +170,7 @@ export default {
       mint: 'Received',
     },
     solWarning:
-      'Your balance may not have enough SOL to cover all transactions. Solana wallets require a minimum of 0.00089088 SOL. We recommend keeping > 0.02 sol in your wallet for the best experience',
+      'Your balance may not have enough SOL to cover all transactions. Solana wallets require a minimum of 0.00089088 SOL. We recommend keeping greater than 0.02 sol in your wallet for the best experience',
     hideFilters: 'Hide Filters',
     myTransactions: 'My Transactions',
     showFilters: 'Show Filters',
