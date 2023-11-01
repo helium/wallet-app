5849ec3ec1b398c6110849a80c6d5668bcec3ae0
bry <bryanzettler@gmail.com>
2023-11-01 15:22:03 -0500

    Bump helium packages and add position actions

--- src/features/governance/PositionCard.tsx
+++ src/features/governance/PositionCard.tsx
@@ -1,27 +1,77 @@
+import BlurActionSheet from '@components/BlurActionSheet'
 import Box from '@components/Box'
 import Text from '@components/Text'
+import TouchableOpacityBox from '@components/TouchableOpacityBox'
+import ListItem from '@components/ListItem'
 import { BoxProps } from '@shopify/restyle'
 import { Theme } from '@theme/theme'
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 interface IPositionCardProps extends Omit<BoxProps<Theme>, 'position'> {
   position: string
 }
 
 export const PositionCard = ({ position, ...boxProps }: IPositionCardProps) => {
+  const [actionsOpen, setActionsOpen] = useState(false)
+  const handleActionPress = () => {
+    setActionsOpen(false)
+  }
+
+  const actions = useCallback(() => {
+    return (
+      <>
+        <ListItem
+          key="split"
+          title="Split"
+          onPress={handleActionPress}
+          selected={false}
+          hasPressedState={false}
+        />
+        <ListItem
+          key="transfer"
+          title="Transfer"
+          onPress={handleActionPress}
+          selected={false}
+          hasPressedState={false}
+        />
+        <ListItem
+          key="extend"
+          title="Extend"
+          onPress={handleActionPress}
+          selected={false}
+          hasPressedState={false}
+        />
+      </>
+    )
+  }, [])
+
   return (
-    <Box backgroundColor="surfaceSecondary" borderRadius="l" {...boxProps}>
-      <Box padding="m">
-        <Box
-          flexDirection="row"
-          justifyContent="space-between"
-          marginBottom="m"
-        >
-          <Text variant="subtitle1" color="primaryText" fontSize={16}>
-            {`Position ${position}`}
-          </Text>
+    <>
+      <TouchableOpacityBox
+        backgroundColor="surfaceSecondary"
+        borderRadius="l"
+        onPress={() => setActionsOpen(true)}
+        {...boxProps}
+      >
+        <Box padding="m">
+          <Box
+            flexDirection="row"
+            justifyContent="space-between"
+            marginBottom="m"
+          >
+            <Text variant="subtitle1" color="primaryText" fontSize={16}>
+              {`Position ${position}`}
+            </Text>
+          </Box>
         </Box>
-      </Box>
-    </Box>
+      </TouchableOpacityBox>
+      <BlurActionSheet
+        title="Position Actions"
+        open={actionsOpen}
+        onClose={() => setActionsOpen(false)}
+      >
+        {actions()}
+      </BlurActionSheet>
+    </>
   )
 }
