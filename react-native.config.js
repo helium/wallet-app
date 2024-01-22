53f0eef9a4b58a82ab0ce57158e33c64b7564cc4
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2024-01-22 11:58:50 -0500

    Add ability to remove flipper (#621)

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   assets: ['./src/assets/fonts/'],
+  dependencies: {
+    ...(process.env.NO_FLIPPER
+      ? { 'react-native-flipper': { platforms: { ios: null } } }
+      : {}),
+  },
 }
