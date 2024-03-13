2908c55198e49d7c66c9e245fc6e962700363ae0
Noah Prince <chewinglass123@gmail.com>
2024-03-13 11:31:02 -0700

    Change transfer to transfer hotspot

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -287,6 +287,7 @@ export default {
       },
       viewInExplorer: 'View in Explorer',
       assertLocation: 'Assert Location',
+      transferHotspot: 'Transfer Hotspot',
       antennaSetup: 'Antenna Setup',
     },
   },
