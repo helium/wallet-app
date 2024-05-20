aff041048ffb03c82349b5273370ae6e4f265b83
Bryan <bryanzettler@gmail.com>
2024-05-20 11:32:45 -0500

    Fix method call for < android 28 (#736)
    * Fix method call for < android 28
    
    * use string concatenation
    
    * tweak

--- android/app/src/main/java/com/helium/wallet/app/MainApplication.java
+++ android/app/src/main/java/com/helium/wallet/app/MainApplication.java
@@ -88,8 +88,17 @@ public class MainApplication extends Application implements ReactApplication {
       PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
 
       String appName = (String) packageManager.getApplicationLabel(applicationInfo);
-      Long appVersionCode = packageInfo.getLongVersionCode();
-      return appName + "/" + appVersionCode.toString() + " " + "android/" + Build.VERSION.RELEASE;
+      Long appVersionCode;
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        // For API level 28 and above
+        appVersionCode = packageInfo.getLongVersionCode();
+      } else {
+        // For lower API levels
+        appVersionCode = (long) packageInfo.versionCode;
+      }
+
+      return appName + "$" + appVersionCode.toString() + " " + "android/" + Build.VERSION.RELEASE;
     } catch (PackageManager.NameNotFoundException e) {
       return null;
     }
