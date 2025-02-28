7ef3aeea4ff8370ee0506786c9488e347d821aee
bry <bryanzettler@gmail.com>
2025-02-28 14:25:17 -0600

    fix en.ts

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1381,8 +1381,7 @@ export default {
       actionsTitle: 'Manage Position',
       longerLockup:
         'Select a new lockup period longer than or equal to the existing {{existing}}',
-      delegateBlurb:
-        "Delegating to a subnetwork may earn you rewards in HNT.",
+      delegateBlurb: 'Delegating to a subnetwork may earn you rewards in HNT.',
       splitWarning:
         'Splitting a Landrush position after the Landrush period will result in the split tokens losing the multiplier!',
       initialVotePowerMult: 'Initial Vote Power Multiplier',
