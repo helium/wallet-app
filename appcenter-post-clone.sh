022743611af08cb937e6e1723906e51b987f3e34
Abhay Kumar <abhay@helium.com>
2025-05-09 09:41:03 -0700

    Remove old appcenter

--- appcenter-post-clone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ./ios/Podfile.lock`
-echo "Installing CocoaPods version $COCOAPODS_VER"
-sudo gem install cocoapods -v $COCOAPODS_VER
