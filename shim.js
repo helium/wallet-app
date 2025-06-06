7cea47f354763e33ddbfe15e665ddb857d210e2a
Noah Prince <chewinglass123@gmail.com>
2025-06-06 10:16:16 -0700

    Fix axios exploding because it sucks

--- shim.js
+++ shim.js
@@ -18,6 +18,9 @@ if (typeof process === 'undefined') {
 process.browser = false
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
+// Required or axios stupidly explodes
+window.location = window.location || {}
+
 // global.location = global.location || { port: 80 }
 const isDev = typeof __DEV__ === 'boolean' && __DEV__
 process.env.NODE_ENV = isDev ? 'development' : 'production'
