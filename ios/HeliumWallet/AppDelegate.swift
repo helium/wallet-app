d3d43dc9468bb795ab114fd86f97bf5545046842
bry <bryanzettler@gmail.com>
2025-07-23 11:38:46 -0500

    wip

--- ios/HeliumWallet/AppDelegate.swift
+++ ios/HeliumWallet/AppDelegate.swift
@@ -5,18 +5,38 @@ import ReactAppDependencyProvider
 import ExpoModulesCore
 
 @main
-class AppDelegate: EXAppDelegateWrapper {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    self.moduleName = "HeliumWallet"
-    self.dependencyProvider = RCTAppDependencyProvider()
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
 
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
-    self.initialProps = [:]
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    factory.startReactNative(
+      withModuleName: "HeliumWallet",
+      in: window,
+      launchOptions: launchOptions
+    )
+
+    return true
   }
 
+  func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    return reactNativeFactory?.application(application, open: url, options: options) ?? false
+}
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
@@ -28,11 +48,11 @@ class AppDelegate: EXAppDelegateWrapper {
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
   }
-  
+
   override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
       return RCTLinkingManager.application(app, open: url, options: options)
   }
-  
+
   override func application(_ application: UIApplication,
                           continue userActivity: NSUserActivity,
                           restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
