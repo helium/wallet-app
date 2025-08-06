ba10f30d355afda8dd7969f464ebed00e91e198d
bry <bryanzettler@gmail.com>
2025-08-06 16:11:23 -0500

    rever expo changes

--- ios/HeliumWallet/AppDelegate.swift
+++ ios/HeliumWallet/AppDelegate.swift
@@ -1,47 +1,21 @@
-import Expo
+import UIKit
 import React
+import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import ExpoModulesCore
 
-@UIApplicationMain
-class AppDelegate: ExpoAppDelegate {
-  var window: UIWindow?
+@main
+class AppDelegate: EXAppDelegateWrapper {
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    self.moduleName = "HeliumWallet"
+    self.dependencyProvider = RCTAppDependencyProvider()
 
-  var reactNativeDelegate: ExpoReactNativeFactoryDelegate?
-  var reactNativeFactory: RCTReactNativeFactory?
-
-  public override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-  ) -> Bool {
-    let delegate = ReactNativeDelegate()
-    let factory = ExpoReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
-
-    reactNativeDelegate = delegate
-    reactNativeFactory = factory
-    bindReactNativeFactory(factory)
-
-    window = UIWindow(frame: UIScreen.main.bounds)
-
-    factory.startReactNative(
-      withModuleName: "HeliumWallet",
-      in: window,
-      launchOptions: launchOptions
-    )
+    // You can add your custom initial props in the dictionary below.
+    // They will be passed down to the ViewController used by React Native.
+    self.initialProps = [:]
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
-  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    return RCTLinkingManager.application(app, open: url, options: options)
-  }
-
-  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    return RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
-  }
-}
-
-class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
@@ -52,5 +26,15 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
 #else
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      return RCTLinkingManager.application(app, open: url, options: options)
+  }
+
+  override func application(_ application: UIApplication,
+                          continue userActivity: NSUserActivity,
+                          restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    return RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
 }
