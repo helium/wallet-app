1abaaef71a207afac5685aac48995f5ac08c2ea5
bry <bryanzettler@gmail.com>
2025-09-12 10:00:49 -0500

    do the ref forwarding at box level

--- src/components/AnimatedBox.tsx
+++ src/components/AnimatedBox.tsx
@@ -1,16 +1,10 @@
-import { Animated, View } from 'react-native'
+import { Animated } from 'react-native'
 import ReAnimated from 'react-native-reanimated'
-import { createBox } from '@shopify/restyle'
-import { Theme } from '@theme/theme'
+import Box from './Box'
 import BlurBox from './BlurBox'
 
-// Create animated components from base components instead of Restyle components
-const AnimatedView = Animated.createAnimatedComponent(View)
-const ReAnimatedView = ReAnimated.createAnimatedComponent(View)
-
-// Create Restyle boxes from the animated components
-const AnimatedBox = createBox<Theme>(AnimatedView)
-export const ReAnimatedBox = createBox<Theme>(ReAnimatedView)
+const AnimatedBox = Animated.createAnimatedComponent(Box)
+export const ReAnimatedBox = ReAnimated.createAnimatedComponent(Box)
 export const ReAnimatedBlurBox = ReAnimated.createAnimatedComponent(BlurBox)
 
 export default AnimatedBox
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,6 +1,16 @@
-import { createBox } from '@shopify/restyle'
+import React, { forwardRef } from 'react'
+import { View } from 'react-native'
+import { createBox, BoxProps } from '@shopify/restyle'
 import { Theme } from '@theme/theme'
 
-const Box = createBox<Theme>()
+const BaseBox = createBox<Theme>()
+
+const Box = forwardRef<View, BoxProps<Theme> & { children?: React.ReactNode }>(
+  (props, ref) => {
+    return <BaseBox ref={ref} {...props} />
+  },
+)
+
+Box.displayName = 'Box'
 
 export default Box
