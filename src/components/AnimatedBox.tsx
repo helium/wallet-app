cc35ac835bbcc2ca59a55d28976828cfb3a647bd
bry <bryanzettler@gmail.com>
2025-09-12 09:46:47 -0500

    use proper view for ReAnimatedBox

--- src/components/AnimatedBox.tsx
+++ src/components/AnimatedBox.tsx
@@ -1,10 +1,16 @@
-import { Animated } from 'react-native'
+import { Animated, View } from 'react-native'
 import ReAnimated from 'react-native-reanimated'
-import Box from './Box'
+import { createBox } from '@shopify/restyle'
+import { Theme } from '@theme/theme'
 import BlurBox from './BlurBox'
 
-const AnimatedBox = Animated.createAnimatedComponent(Box)
-export const ReAnimatedBox = ReAnimated.createAnimatedComponent(Box)
+// Create animated components from base components instead of Restyle components
+const AnimatedView = Animated.createAnimatedComponent(View)
+const ReAnimatedView = ReAnimated.createAnimatedComponent(View)
+
+// Create Restyle boxes from the animated components
+const AnimatedBox = createBox<Theme>(AnimatedView)
+export const ReAnimatedBox = createBox<Theme>(ReAnimatedView)
 export const ReAnimatedBlurBox = ReAnimated.createAnimatedComponent(BlurBox)
 
 export default AnimatedBox
