0a05be09066c34bfe699dc10e41923c442380c35
bry <bryanzettler@gmail.com>
2025-09-12 10:07:50 -0500

    allow style passthrough

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,15 +1,16 @@
 import React, { forwardRef } from 'react'
-import { View } from 'react-native'
+import { View, ViewStyle } from 'react-native'
 import { createBox, BoxProps } from '@shopify/restyle'
 import { Theme } from '@theme/theme'
 
 const BaseBox = createBox<Theme>()
 
-const Box = forwardRef<View, BoxProps<Theme> & { children?: React.ReactNode }>(
-  (props, ref) => {
-    return <BaseBox ref={ref} {...props} />
-  },
-)
+const Box = forwardRef<
+  View,
+  BoxProps<Theme> & { children?: React.ReactNode; style?: ViewStyle }
+>((props, ref) => {
+  return <BaseBox ref={ref} {...props} />
+})
 
 Box.displayName = 'Box'
 
